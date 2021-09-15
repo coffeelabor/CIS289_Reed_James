79f04dd21f2fe6710bb69400cd5db555ded1b288
Reed James <reedtjames1234@gmail.com>
2021-09-14 21:55:55 -0500

    files created w/ docstring

---
+++ Unit_3/file_io_analysis.py
@@ -0,0 +1,18 @@
+'''
+/***************************************************************
+* Name: File I/O and Analysis Program
+* Author: Reed James
+* Created: 15 Sept 2021
+* Course: CIS 289 - Python
+* Version: Python 3.8.2
+* OS: Windows 10
+* Copyright: This is my own original work based on
+*               specifications issued by our instructor
+* Description:
+*               Input:
+*               Output:
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or
+* unmodified. I have not given other fellow student(s) access to my program.
+***************************************************************/
+'''
