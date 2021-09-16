7e551c14fd4d247e3849fc6853a162edb32dfb0b
Reed James <reedtjames1234@gmail.com>
2021-09-15 23:04:57 -0500

    unit 3 homework done

--- Unit_3/file_io_analysis.py
+++ Unit_3/file_io_analysis.py
@@ -8,11 +8,65 @@
 * OS: Windows 10
 * Copyright: This is my own original work based on
 *               specifications issued by our instructor
-* Description:
-*               Input:
-*               Output:
+* Description: program that looks at files
+*               Input: Moby dick, Sense and Sensibility
+*               Output: Word analysis
 * Academic Honesty: I attest that this is my original work.
 * I have not used unauthorized source code, either modified or
 * unmodified. I have not given other fellow student(s) access to my program.
 ***************************************************************/
 '''
+import re
+
+def regex_helper(w_to_search,in_line):
+    clean_line = re.sub(r'[^\w\s]', '', in_line)
+    result = len(re.findall(f"\s{w_to_search}\s", clean_line, flags=re.IGNORECASE))
+    return result
+
+def search_helper(w_to_search, in_file):
+    count = 0
+    for line in in_file:
+        if(regex_helper(w_to_search, line) == 0):
+            continue
+        else:
+            count = count + regex_helper(w_to_search, line)
+    return count
+
+def find_word_old(in_file):
+    word = 'old'
+    result = search_helper(word, in_file)
+    print(f'The word "{word}" appears {result} times')
+
+
+def find_word_water(in_file):
+    word = 'water'
+    result = search_helper(word, in_file)
+    print(f'The word "{word}" appears {result} times')
+
+
+
+def find_average_sentence_length(in_file):
+    # clean_file = re.sub(r'[^\w\s]', '', in_file)
+    # split_file = clean_file.split('.')
+    # split_file = in_file.split('.')
+    # print('test', split_file)
+    pass
+
+
+
+if __name__ == "__main__":
+    with open('Moby_Dick_Chapter_1.txt', 'r', encoding='utf-8') as input_file:
+        print("OUTPUT 1 Moby Dick")
+        find_word_old(input_file)
+    with open('Moby_Dick_Chapter_1.txt', 'r', encoding='utf-8') as input_file:
+        print("\nOUTPUT 2 Moby Dick")
+        find_word_water(input_file)
+    with open('Moby_Dick_Chapter_1.txt', 'r', encoding='utf-8') as input_file:
+        print("\nOUTPUT 3 Moby Dick")
+        # find_average_sentence_length(input_file)
+    with open('Sense_and_Sensibility_Chapter_1.txt', 'r', encoding='utf-8') as input_file:
+        print("\nOUTPUT 4 Sense and Sensibility")
+        find_word_old(input_file)
+    with open('Sense_and_Sensibility_Chapter_1.txt', 'r', encoding='utf-8') as input_file:
+        print("\nOUTPUT 5 Sense and Sensibility")
+        find_word_water(input_file)
--- Unit_3/numpy_array_math.py
+++ Unit_3/numpy_array_math.py
@@ -8,11 +8,57 @@
 * OS: Windows 10
 * Copyright: This is my own original work based on
 *               specifications issued by our instructor
-* Description:
-*               Input:
-*               Output:
+* Description: program that looks at numpy arrays
+*               Input: two 2d arrays
+*               Output: different properties of each array
 * Academic Honesty: I attest that this is my original work.
 * I have not used unauthorized source code, either modified or
 * unmodified. I have not given other fellow student(s) access to my program.
 ***************************************************************/
 '''
+import numpy as np
+
+def print_arr(arr):
+    print('Array: \n',arr)
+
+def print_shape(arr):
+    print('Shape: \n', arr.shape)
+
+def print_two_by_two(arr):
+    result = arr[0:2, 0:2]
+    print('Two x Two \n', result)
+
+def print_bool_value(arr):
+    result = np.where(arr % 2 == 0, True, np.where((arr+1) % 2 == 0, False, 'Decimal'))
+    print('Bool Val: \n', result)
+
+def sum_elementwise(arr1, arr2):
+    result = np.add(arr1, arr2)
+    print('Summed: \n', result)
+
+def multipy_elementwise(arr1, arr2):
+    result = np.multiply(arr1, arr2)
+    print('Multiplied: \n', result)
+
+def print_sum_elements(arr):
+    print('Sum of Elements: \n\t', arr.sum())
+
+def print_product_elements(arr):
+    print('Product of Elements: \n\t', arr.prod())
+
+def print_max_min(arr):
+    print(f'Max and Min\n\tMax: {arr.max()}\n\tMin: {arr.min()}')
+
+if __name__ == "__main__":
+    arr_1 = np.array([[10,15,20],[2,3,4],[9,14.5,18]])
+    arr_2 = np.array([[1,2,5],[8,0,12],[11,3,22]])
+
+    print_arr(arr_1)
+    print_shape(arr_1)
+    print_two_by_two(arr_1)
+    print_bool_value(arr_1)
+    sum_elementwise(arr_1,arr_2)
+    multipy_elementwise(arr_1, arr_2)
+    print_sum_elements(arr_2)
+    print_product_elements(arr_2)
+    print_max_min(arr_2)
