6d1bc126d6b50411f2bad1d65cfeebd47b68a6ff
Reed James <reedtjames1234@gmail.com>
2021-10-09 15:40:49 -0500

    .

---
+++ Unit_6/Plot_From_Dataset.py
@@ -0,0 +1,26 @@
+'''
+/***************************************************************
+* Name: Plot From Dataset
+* Author: Reed James
+* Created: 6 Oct 2021
+* Course: CIS 289 - Python
+* Version: Python 3.8.2
+* OS: Windows 10
+* Copyright: This is my own original work based on
+*               specifications issued by our instructor
+* Description: 
+*               Input: 
+*               Output: 
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or
+* unmodified. I have not given other fellow student(s) access to my program.
+***************************************************************/
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+# from matplotlib import pyplot as plt
+
+if __name__ == "__main__":
+    print('test')
+    plt.rcParames['figure.dpi'] = 150
